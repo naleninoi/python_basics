adde459e4b692bd9612eb217d39f09eece71f9e0
Viktor <61115892+naleninoi@users.noreply.github.com>
2020-02-25 20:06:23 +0300

    Update 1.2_seconds.py

--- 1.2_seconds.py
+++ 1.2_seconds.py
@@ -4,4 +4,4 @@ hours = time // 3600
 minutes = (time - (hours * 3600)) // 60
 seconds = time - (hours * 3600) - (minutes * 60)
 
-print (f'В формате чч:мм:сс ---> {hours}:{minutes}:{seconds}')
+print (f'В формате чч:мм:сс ---> {hours:02}:{minutes:02}:{seconds:02}')
