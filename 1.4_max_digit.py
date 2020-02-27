3138820bbdca6b227ad3e581288e791af9cf18cc
Viktor <61115892+naleninoi@users.noreply.github.com>
2020-02-27 14:52:15 +0300

    Delete 1.4_max_digit.py

--- 1.4_max_digit.py
+++
@@ -1,9 +0,0 @@
-number = int(input('Введите целое положительное число: '))
-
-i = 0
-while number > 0:
-    if (number % 10) > i:
-        i = number % 10
-    number = number // 10
-
-print('Наибольшая цифра в числе - %d' % (i))
