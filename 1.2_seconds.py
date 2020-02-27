8ef17f609e9b74f69b1397afcb32ccde9daf5071
Viktor <61115892+naleninoi@users.noreply.github.com>
2020-02-27 14:51:59 +0300

    Delete 1.2_seconds.py

--- 1.2_seconds.py
+++
@@ -1,7 +0,0 @@
-time = int(input('Введите количество секунд: '))
-
-hours = time // 3600
-minutes = (time - (hours * 3600)) // 60
-seconds = time - (hours * 3600) - (minutes * 60)
-
-print (f'В формате чч:мм:сс ---> {hours:02}:{minutes:02}:{seconds:02}')
